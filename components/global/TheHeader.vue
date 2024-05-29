5e9a800757c43f1d584c3262c530e80c108595f9
kattata <kasia.laniecka@gmail.com>
2024-05-29 21:54:26 +0200

    Header line

--- components/global/TheHeader.vue
+++ components/global/TheHeader.vue
@@ -1,47 +1,57 @@
 <script lang="ts" setup></script>
 
 <template>
-  <div class="container">
-    <header class="header">
-      <BaseLink to="/" class="header__logo">
-        {{ $t('Header.Logo') }}
-      </BaseLink>
-      <div class="divider divider--horizontal"></div>
-      <ul>
-        <li>
-          <BaseLink to="#about">
-            {{ $t('Header.NavigationItems.About') }}
-          </BaseLink>
-        </li>
-        <li>
-          <BaseLink to="#skills">
-            {{ $t('Header.NavigationItems.Skills') }}
-          </BaseLink>
-        </li>
-        <li>
-          <BaseLink to="#experience">
-            {{ $t('Header.NavigationItems.Experience') }}
-          </BaseLink>
-        </li>
-      </ul>
-    </header>
-  </div>
+  <header class="header">
+    <div class="divider divider--horizontal"></div>
+
+    <div class="container container--no-padding">
+      <div class="header__inner">
+        <BaseLink to="/" class="header__logo">
+          {{ $t('Header.Logo') }}
+        </BaseLink>
+        <ul>
+          <li>
+            <BaseLink to="#about">
+              {{ $t('Header.NavigationItems.About') }}
+            </BaseLink>
+          </li>
+          <li>
+            <BaseLink to="#skills">
+              {{ $t('Header.NavigationItems.Skills') }}
+            </BaseLink>
+          </li>
+          <li>
+            <BaseLink to="#experience">
+              {{ $t('Header.NavigationItems.Experience') }}
+            </BaseLink>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </header>
 </template>
 
 <style lang="postcss" scoped>
 .header {
-  margin-top: 36px;
-  display: flex;
-  gap: 16px;
-  justify-content: space-between;
-  align-items: center;
-  text-transform: uppercase;
+  margin-top: 20px;
+  position: relative;
+
+  &__inner {
+    z-index: 10;
+    position: relative;
+    gap: 16px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    text-transform: uppercase;
+  }
 
   ul {
-    padding: 0;
     margin: 0;
     display: flex;
     gap: 20px;
+    background-color: var(--color-secondary-100);
+    padding: 16px;
 
     li {
       list-style-type: none;
@@ -50,6 +60,15 @@
 
   &__logo {
     white-space: nowrap;
+    background-color: var(--color-secondary-100);
+    padding: 16px;
+  }
+
+  .divider {
+    position: absolute;
+    left: 0;
+    top: 50%;
+    transform: translateY(-50%);
   }
 }
 </style>
