abc33b72d0b9175d1d691bb2950d5f9e3009ed1a
kattata <kasia.laniecka@gmail.com>
2024-10-19 19:14:47 +0200

    Remove old files

--- utils/navigation.ts
+++
@@ -1,6 +0,0 @@
-import { getNavigationItems } from '~/content/ts/navigation';
-import type { NavigationItemEnum, NavigationType } from '~/types/navigation';
-
-export function getNavigationItemById(id: NavigationItemEnum) {
-  return getNavigationItems().find((item: NavigationType) => item.id === id);
-}
